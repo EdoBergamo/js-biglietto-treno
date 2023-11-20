d8b1b8bdb6e1c4b39b94c637083d2b0b6a70cfa6
EdoBergamo <bergamo@edoardo-hb.eu>
2023-11-20 22:05:37 +0100

    [UPDATE] Added script.js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Inizializzazione variabili
+let km = prompt("Quanti km devi effettuare?");
+let age = prompt("Quanti anni hai?");
+let price = km * 0.21;
+let discount = 0;
+
+// Controllo età ed assegno corrispettivo sconto
+if (age < 18) {
+  discount = (price * 20) / 100;
+} else if (age >= 65) {
+  discount = (price * 40) / 100;
+}
+
+let discounted_price = price - discount;
+
+// Mostro il risultato a video
+document.getElementById("prezzo").innerHTML = `Il costo del biglietto è ${discounted_price.toFixed()}`
